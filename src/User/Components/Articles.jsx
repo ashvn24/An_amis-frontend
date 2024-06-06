bc022924ac3257996abbbe56bde6c63967ed2a06
Ashwin vk <133885217+ashvn24@users.noreply.github.com>
2024-06-06 15:43:51 +0530

    Update Articles.jsx

--- src/User/Components/Articles.jsx
+++ src/User/Components/Articles.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import yel from '../../assets/product/yel.jpg'
 import prod from '../../assets/product/prods.jpg'
 import port from '../../assets/product/port.jpg'
+import bot from '.../.../assets/product/prod.jpeg'
 
 const Articles = () => {
   return (
     <div>
       <div className="p-4 grid grid-cols-1 md:grid-cols-4 md:grid-rows-2 gap-4 md:h-screen">
         <div className="h-64 md:h-auto md:col-span-2 md:row-span-2 relative">
-          <img src={port} className="absolute inset-0 w-full h-full object-cover" alt="Cleansers" />
+          <img src={bot} className="absolute inset-0 w-full h-full object-cover" alt="Cleansers" />
         </div>
         <div className="text-gray-700 text-center md:col-span-2 flex items-center justify-center">
           <div className="relative z-10 p-12">
